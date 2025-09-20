dc0d89c1346b0ee3dc3b9f4b0002a3f8b918a58d
chan9yu <dev.cgyeo@gmail.com>
2025-09-20 14:38:43 +0900

    chore: init tailwind

--- src/ModalFormPage.tsx
+++ src/ModalFormPage.tsx
@@ -1,6 +1,10 @@
 const ModalFormPage = () => {
   /* 여기에 구현해 주세요 */
-  return <div>ModalFormPage</div>;
+  return (
+    <div>
+      <h2 className="text-2xl">ModalFormPage</h2>
+    </div>
+  );
 };
 
 export default ModalFormPage;
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,9 @@
 import "modern-normalize";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+
 import ModalFormPage from "./ModalFormPage";
+import "./styles.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
