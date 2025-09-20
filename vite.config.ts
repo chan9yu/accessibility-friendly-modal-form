7b39abde597cfc75dcee978bba4defad87eb9072
chan9yu <dev.cgyeo@gmail.com>
2025-09-20 14:41:31 +0900

    chore: vitest 환경구성

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,11 @@
-import { defineConfig } from 'vite';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [tailwindcss()],
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: ["./src/test/setup.ts"],
+  },
 });
